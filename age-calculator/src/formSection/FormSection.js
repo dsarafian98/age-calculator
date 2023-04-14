5bbb54c29fdffa4224424e9a3e0eaa2866785fb9
k16ds01 <dsarafian98@gmail.com>
2023-04-14 11:27:56 -0400

    add formsection

---
+++ age-calculator/src/formSection/FormSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FormSection.css";
+import { TextField } from "@material-ui/core";
+
+const FormSection = () => {
+  return (
+    <div>
+      <TextField label="hi"></TextField>
+    </div>
+  );
+};
+
+export default FormSection;
